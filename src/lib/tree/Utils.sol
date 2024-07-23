d451808a1e6a2ac2607e487d42588ae2c992928c
sweexordious <chamirachid1@gmail.com>
2024-07-23 21:16:48 +0100

    chore: re-introduce the _

--- src/lib/tree/Utils.sol
+++ src/lib/tree/Utils.sol
@@ -7,7 +7,7 @@ import "./Constants.sol";
 /// @param numLeaves : The total number of leaves in the tree
 /// @return startingBit : The starting bit of the path
 // solhint-disable-next-line func-visibility
-function getStartingBit(uint256 numLeaves) pure returns (uint256 startingBit) {
+function getStartingBit(uint256 numLeaves) private pure returns (uint256 startingBit) {
     // Determine height of the left subtree. This is the maximum path length, so all paths start at this offset from the right-most bit
     startingBit = 0;
     while ((1 << startingBit) < numLeaves) {
@@ -21,7 +21,7 @@ function getStartingBit(uint256 numLeaves) pure returns (uint256 startingBit) {
 /// @param numLeaves: The total number of leaves in the tree
 /// @return pathLength : The length of the path to the leaf
 // solhint-disable-next-line func-visibility
-function pathLengthFromKey(uint256 key, uint256 numLeaves) pure returns (uint256 pathLength) {
+function pathLengthFromKey(uint256 key, uint256 numLeaves) private pure returns (uint256 pathLength) {
     if (numLeaves <= 1) {
         // if the number of leaves of the tree is 1 or 0, the path always is 0.
         return 0;
@@ -49,7 +49,7 @@ function pathLengthFromKey(uint256 key, uint256 numLeaves) pure returns (uint256
 /// @notice Returns the minimum number of bits required to represent `x`; the
 /// result is 0 for `x` == 0.
 /// @param x Number.
-function _bitsLen(uint256 x) pure returns (uint256) {
+function _bitsLen(uint256 x) private pure returns (uint256) {
     uint256 count = 0;
 
     while (x != 0) {
@@ -62,7 +62,7 @@ function _bitsLen(uint256 x) pure returns (uint256) {
 
 /// @notice Returns the largest power of 2 less than `x`.
 /// @param x Number.
-function _getSplitPoint(uint256 x) pure returns (uint256) {
+function _getSplitPoint(uint256 x) private pure returns (uint256) {
     // Note: since `x` is always an unsigned int * 2, the only way for this
     // to be violated is if the input == 0. Since the input is the end
     // index exclusive, an input of 0 is guaranteed to be invalid (it would
@@ -81,8 +81,8 @@ function _getSplitPoint(uint256 x) pure returns (uint256) {
 /// not overlap `end`.
 /// @param begin Begin index, inclusive.
 /// @param end End index, exclusive.
-function nextSubtreeSize(uint256 begin, uint256 end) pure returns (uint256) {
-    uint256 ideal = bitsTrailingZeroes(begin);
+function _nextSubtreeSize(uint256 begin, uint256 end) private pure returns (uint256) {
+    uint256 ideal = _bitsTrailingZeroes(begin);
     uint256 max = _bitsLen(end - begin) - 1;
     if (ideal > max) {
         return 1 << max;
@@ -93,7 +93,7 @@ function nextSubtreeSize(uint256 begin, uint256 end) pure returns (uint256) {
 /// @notice Returns the number of trailing zero bits in `x`; the result is
 /// 256 for `x` == 0.
 /// @param x Number.
-function bitsTrailingZeroes(uint256 x) pure returns (uint256) {
+function _bitsTrailingZeroes(uint256 x) private pure returns (uint256) {
     uint256 mask = 1;
     uint256 count = 0;
 
